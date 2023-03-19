bc8f3be63bf747c3f2a7da13da50af822a8a110d
pradyun <pradyun.setti@gmail.com>
2023-03-19 17:48:09 +1300

    First Commit

---
+++ source/Level/Level.hpp
@@ -0,0 +1,16 @@
+/*
+    Level.hpp
+    Bakery2D
+    Created by breadone
+*/
+
+#pragma once
+
+#ifndef LEVEL_HPP
+#define LEVEL_HPP
+
+class Level {
+
+};
+
+#endif
---
+++ source/main.cpp
@@ -0,0 +1,6 @@
+// this file (and the makefile) exists purely to test compilation, it has no real use
+
+#include "Sprite/Sprite.hpp"
+#include "Level/Level.hpp"
+
+int main(int argc, char** argv) { return 0; }
